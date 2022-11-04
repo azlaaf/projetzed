4df059bf634a7692f013284697090997819e385e
azlaf <zakaria-azlaf@hotmail.com>
2022-11-04 18:54:33 +0100

    create-react-application

---
+++ azlaf/frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/"> azlaf </a>
+      </header>
+      <main> list product </main>
+    </div>
+  );
+}
+
+export default App;
